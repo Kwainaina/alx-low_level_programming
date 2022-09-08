24b117c8e97e13e58fcdc1f9658478bdf1156ca2
Kwainaina <kwainaina7@gmail.com>
2022-09-08 09:58:10 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * Descrption: main - prints out a string to stdout.
+ * Descrption: main - Prints out a string to stdout.
  * Return: 0 if success.
- * */
+ */
 int main(void)
 {
 puts("programming is like building a multilingual puzzle");
