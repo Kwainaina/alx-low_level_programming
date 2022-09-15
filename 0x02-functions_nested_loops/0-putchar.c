5693a0f172099f19de989321798c23c5de4f2c2e
Kwainaina <kwainaina7@gmail.com>
2022-09-15 01:42:51 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,43 +1,25 @@
-#include "main.h"                                                                                                                                      
+nclude <unistd.h>
 
-                                                                                                                                                       
 
-/**                                                                                                                                                    
 
- * main- print putchar                                                                                                                                 
+/**
+ *
+ *  * _putchar - writes the character c to stdout
+ *
+ *   * @c: The character to print
+ *
+ *    *
+ *
+ *     * Return: On success 1.
+ *
+ *      * On error, -1 is returned, and errno is set appropriately.
+ *
+ *       */
 
- * Return: always 0                                                                                                                                    
+int _putchar(char c)
 
- */                                                                                                                                                    
+{
 
-                                                                                                                                                       
+		return (write(1, &c, 1));
 
-int main(void)                                                                                                                                         
-
-{                                                                                                                                                      
-
-        _putchar('_');                                                                                                                                 
-
-        _putchar('p');                                                                                                                                 
-
-        _putchar('u');                                                                                                                                 
-
-        _putchar('t');                                                                                                                                 
-
-        _putchar('c');                                                                                                                                 
-
-        _putchar('h');                                                                                                                                 
-
-        _putchar('a');                                                                                                                                 
-
-        _putchar('r');                                                                                                                                 
-
-        _putchar('\n');                                                                                                                                
-
-                                                                                                                                                       
-
-        return (0);                                                                                                                                    
-
-root@e574a84705eb:/alx-low_level_programming/0x02-functions_nested_loops# git add .
-root@e574a84705eb:/alx-low_level_programming/0x02-functions_nested_loops# git commit -m 'putchar'
-root@e574a84705eb:/alx-low_level_programming/0x02-functions_nested_loops# git push
+}
