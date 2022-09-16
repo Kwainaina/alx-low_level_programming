3b1c9299e258d64a96678e54ab09e9b71cdcee87
Kwainaina <kwainaina7@gmail.com>
2022-09-16 13:20:42 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,31 +8,20 @@
  */
 int main(void)
 {
-	int n;
+	int num;
 
-	for (n = 1; n <= 100; n++)
+	for (num = 1; num <= 100; num++)
 	{
-	if ((n % 3) == 0 && (n % 5) == 0)
-	{
-	printf("FizzBuzz");
-	}
-	else if ((n % 3) == 0)
-	{
-	printf("Fizz");
-	}
-	else if ((n % 5) == 0)
-	{
-	printf("Buzz");
-	}
-	else
-	{
-	printf("%d", n);
-	}
-	if (n == 100)
-	{
-	continue;
-	}
-	printf(" ")
+		if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
+		else if (num % 3 == 0 && num % 5 != 0)
+			printf("Fizz");
+		else if (num == 100)
+			printf("Buzz");
+		else if (num % 5 == 0 && num % 3 != 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
 	}
 	printf("\n");
 	return (0);
