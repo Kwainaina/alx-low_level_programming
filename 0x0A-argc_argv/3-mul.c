86a262858b8f2fbdb10788ff9ccad4f64fa55677
Kwainaina <kwainaina7@gmail.com>
2022-09-30 04:39:29 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,22 +6,20 @@
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: 0 for success
+ * Return: 0 if true, 1 if false
  */
 
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int a, b;
 
-	if (argc != 3)
+	if (argc == 1)
 	{
-		printf("%s\n", "Error");
-		return (1);
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+		return (0);
 	}
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
-	return (0);
+	printf("Error\n");
+	return (1);
 }
