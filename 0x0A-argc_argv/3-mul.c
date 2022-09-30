4a87e13d111b5b6fd5d1b3f3cbe629d1cee55e2c
Kwainaina <kwainaina7@gmail.com>
2022-09-30 04:11:24 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,8 +18,8 @@ int main(int argc, char *argv[])
 		printf("%s\n", "Error");
 		return (1);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 
 	printf("%d\n", result);
