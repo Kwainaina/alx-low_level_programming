b4762d44c21543f36f0a1075571d55abb9b36c73
Kwainaina <kwainaina7@gmail.com>
2022-09-30 03:23:31 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,29 +11,17 @@
 
 int main(int argc, char *argv[])
 {
-	int count, num1, num2, res;
+	int result, num1, num2;
 
-	count = num1 = num2 = res = 0;
-	if (argc > 0)
+	if (argc != 3)
 	{
-		while (count < argc)
-		{
-			if (argc == 3)
-			{
-				if (count == 1)
-					num1 = atoi(argv[count]);
-				else if (count == 2)
-					num2 = atoi(argv[count]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-			res = (num1 * num2);
-			count++;
-		}
-		printf("%d\n", res);
+		printf("%s\n", "Error");
+		return (1);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
 	return (0);
 }
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that adds positive numbers.
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * return: int
+ */
+
+int main(int argc, char *argv[])
+{
+
