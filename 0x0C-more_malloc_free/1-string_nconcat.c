41dc93953b5ce2da73e3ef4f937ed24ff84ea393
Kwainaina <kwainaina7@gmail.com>
2022-10-06 03:24:26 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * string_nconcat - function that concatenates two strings
+ * @s1: input string 1
+ * @s2: input string 2
+ * @n: first n bytes
+ * Return: char
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
