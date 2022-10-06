7ec51a82b115820bd70a6fb32aa2e09476cf706e
Kwainaina <kwainaina7@gmail.com>
2022-10-06 09:12:10 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,34 +10,16 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *cal;
+	char *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	cal = malloc(nmemb * size);
-	if (cal == NULL)
+	p = malloc(nmemb * size);
+	if (p == NULL)
 		return (NULL);
-	_memset(cal, 0, nmemb * size);
-	return (cal);
-}
-/**
- * _memset - function that fills memory with a constant type
- *
- * @s: memory area to return
- * @b: constant byte
- * @n: size of bytes
- * Return: char
- */
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
 
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int 1 = 0;
-
-	while (i < n)
-	{
-
-		*(s + i) = b;
-		i += 1;
-	}
-	return (s);
+	return (p);
 }
