38511e895670dd5403807aab20155e1b4071435b
Kwainaina <kwainaina7@gmail.com>
2022-10-10 04:07:35 -0700

    Hello World

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argument count 
+ * @av: argument vector
+ * Return: char
+ */
+
+char *argstostr(int ac, char **av)
+{
+
