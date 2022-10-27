4fa027e30b9e7f2207d1ba8327ac4b5077f52976
kwainaina <kwainaina7@gmail.com>
2022-10-27 08:40:08 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	listint_t *temp2;
 
-	temp2 = *head;
-	while (temp2 != NULL && head != NULL)
+	if (head == NULL)
+		return;
+	while (*head)
 	{
-		temp = temp2;
-		temp2 = temp2->next;
+		temp = *head;
+		*head = (*head)->next;
 		free(temp);
 	}
-	*head = NULL;
+	head = NULL;
 }
