1b5bf671120a8bcb06656fb0816a97926271224b
kwainaina <kwainaina7@gmail.com>
2022-10-31 05:54:05 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,21 +11,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask;
-	unsigned int s_index;
-	(void) index;
+	if (index >= (sizeof(unsigned long int) *))
+		return (-1);
+	*n ^= (1 << index);
 
-	mask = 1;
-	s_index - 0;
-	while (mask < 2147483648)
-	{
-		if (s_index == index)
-		{
-			*n = *n | 1 << index;
-			return (1);
-		}
-		s_index++;
-		mask = mask << 1;
-	}
-	return (-1);
+	return (1);
 }
