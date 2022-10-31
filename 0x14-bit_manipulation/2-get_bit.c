e8d00753bbb11787d93249642901174d0d4aa5c8
kwainaina <kwainaina7@gmail.com>
2022-10-31 05:35:36 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,11 +11,22 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 0))
-		return (-1);
+	unsigned long int mask;
+	unsigned int s_index;
 
-	if ((n & (1 << index)) == 0)
-		return (0);
-
-	return (1);
+	mask = 1;
+	s_index = 0;
+	while (mask < 2147483648)
+	{
+		if (s_index == index)
+		{
+			if ((mask & n) == 0)
+				return (0);
+			else
+				return (1);
+		}
+		s_index++;
+		mask = mask << 1;
+	}
+	return (-1);
 }
