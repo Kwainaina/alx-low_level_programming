1640c0575b0148c1c58efaaa86746ba2812fe916
kwainaina <kwainaina7@gmail.com>
2022-11-02 10:02:33 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,24 +14,18 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = o;
+	int fd, fd_write;
 
 	if (filename == NULL)
 		return (-1);
-
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	if (text_content == NULL)
+		return (1);
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
 		return (-1);
-
-	close(o);
-
+	fd_write = write(fd, text_content, _strlen(text_content));
+	close(fd);
+	if (fd_write == -1)
+		return (-1);
 	return (1);
 }
