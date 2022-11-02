83ee4423aebaebe23aebbc294d4b27946e73d001
kwainaina <kwainaina7@gmail.com>
2022-11-02 10:13:16 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,6 +3,24 @@
 #include "main.h"
 
 /**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: pointer to a string
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i += 1;
+	}
+	return (i);
+}
+
+/**
  * append_text_to_file - appends text at the end of a file
  * @filename: a pointer to the name of the file
  * @text_content: the string to add to the end of the file
